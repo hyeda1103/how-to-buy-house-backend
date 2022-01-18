0e836132589a04e4278ec05ce2dc43c4276d3154
hyeda1103 <dalgona92@gmail.com>
2022-01-18 22:31:05 +0900

    feat: express with typescript, ESLint, prettier boilerplate

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, {
+  Application, Request, Response,
+} from 'express';
+
+const app: Application = express();
+const port = 5000;
+
+app.use('/', (req: Request, res: Response) => {
+  res.status(200).send({ data: 'Hello from Ornio AS' });
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
