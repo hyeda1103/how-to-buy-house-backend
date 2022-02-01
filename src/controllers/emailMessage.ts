ae0eb9130285b2505dd7fbb76b6d96511b78c173
hyeda1103 <dalgona92@gmail.com>
2022-02-01 19:18:10 +0900

    feat: prevent profane words in email

--- src/controllers/emailMessage.ts
+++ src/controllers/emailMessage.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import sgMail from '@sendgrid/mail';
+import Filter from 'bad-words';
 
 import EmailMessage from '../models/emailMessage';
 
@@ -8,6 +9,15 @@ const sendEmailMessage = expressAsyncHandler(async (req: any, res: Response) => 
   sgMail.setApiKey(`${process.env.SENDGRID_API_KEY}`);
 
   const { to, subject, message } = req.body;
+  // Get the message
+  const emailMessage = `${subject} ${message}`;
+  // Prevent profanity/bad words
+  const filter = new Filter({
+    replaceRegex: /[A-Za-z0-9가-힣]/g, // Multilingual support for word filtering
+    list: [], // Add words to the blacklist
+  });
+  const isProfane = filter.isProfane(emailMessage);
+  if (isProfane) throw new Error('이메일에 부적절한 내용이 담겨 있어 발신 실패하였습니다');
   try {
     // Build up message
     const newMessage = {
