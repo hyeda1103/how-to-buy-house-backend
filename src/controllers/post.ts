502271a06c9b57b2d9bbb5169cae43cea4a23cec
hyeda1103 <dalgona92@gmail.com>
2022-02-01 14:13:39 +0900

    feat: post likes controller

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -114,3 +114,50 @@ export const deletePost = expressAsyncHandler(async (req: any, res: Response) =>
     res.json(error);
   }
 });
+
+// @desc    Like a post
+// @route   PUT /api/posts/likes
+// @access  Private
+export const toggleLikeToPost = expressAsyncHandler(async (req: any, res: Response) => {
+  // Find the post to be liked
+  const { postId } = req.body;
+  const post = await Post.findById(postId);
+  // Find the login user
+  const loginUserId = req.user?.id;
+  // Find if this user has liked this post already
+  const { isLiked, disLikes } = post;
+  // Check if this user has disliked this post
+  const alreadyDisLiked = disLikes?.find(
+    (userId: string) => userId?.toString() === loginUserId.toString(),
+  );
+  // Remove the user from dislikes array if exists
+  if (alreadyDisLiked) {
+    const post = await Post.findByIdAndUpdate(postId, {
+      $pull: { disLikes: loginUserId },
+      isDisLiked: false,
+    }, {
+      new: true,
+    });
+    res.json(post);
+  }
+  // Toggle
+  // Remove the user if she/he has liked the post
+  if (isLiked) {
+    const post = await Post.findByIdAndUpdate(postId, {
+      $pull: { likes: loginUserId },
+      isLiked: false,
+    }, {
+      new: true,
+    });
+    res.json(post);
+  } else {
+    // Add to likes
+    const post = await Post.findByIdAndUpdate(postId, {
+      $push: { likes: loginUserId },
+      isLiked: true,
+    }, {
+      new: true,
+    });
+    res.json(post);
+  }
+});
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -8,12 +8,14 @@ import {
   fetchSinglePost,
   updatePost,
   deletePost,
+  toggleLikeToPost,
 } from '../controllers/post';
 
 const postRoutes = express.Router();
 
 postRoutes.post('/', authMiddleware, imageUploadMiddleware.single('image'), PostImageResizeMiddleware, createPost);
 postRoutes.get('/', fetchAllPost);
+postRoutes.put('/likes', authMiddleware, toggleLikeToPost);
 postRoutes.get('/:id', fetchSinglePost);
 postRoutes.put('/:id', authMiddleware, updatePost);
 postRoutes.delete('/:id', deletePost);
