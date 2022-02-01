deea175df384ba8d0c3cfad628da4acbfe817591
hyeda1103 <dalgona92@gmail.com>
2022-02-01 19:41:09 +0900

    feat: fetch all categories controller

--- src/controllers/category.ts
+++ src/controllers/category.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import Category from '../models/category';
 
-const createCategory = expressAsyncHandler((async (req: any, res: Response) => {
+export const createCategory = expressAsyncHandler((async (req: any, res: Response) => {
   try {
     const category = await Category.create({
       user: req.user._id,
@@ -14,4 +14,13 @@ const createCategory = expressAsyncHandler((async (req: any, res: Response) => {
   }
 }));
 
-export default createCategory;
+export const fetchAllCategories = expressAsyncHandler((async (req: any, res: Response) => {
+  try {
+    const allCategories = await Category.find({})
+      .populate('user')
+      .sort('-createdAt');
+    res.json(allCategories);
+  } catch (error) {
+    res.json(error);
+  }
+}));
--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import createCategory from '../controllers/category';
+import { createCategory, fetchAllCategories } from '../controllers/category';
 import authMiddleware from '../middlewares/auth';
 
 const categoryRoutes = express.Router();
 
 categoryRoutes.post('/', authMiddleware, createCategory);
+categoryRoutes.get('/', fetchAllCategories);
 
 export default categoryRoutes;
